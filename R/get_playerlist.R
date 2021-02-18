04a098eb20539909ef72dddedb2fc84a3fb62747
pontsuyu <wu.pe.s2.47@gmail.com>
2021-02-18 12:37:27 +0900

    update playerlist, fg_leaders

--- R/get_playerlist.R
+++ R/get_playerlist.R
@@ -5,5 +5,6 @@
 #' \dontrun{get_playerlist()}
 #' 
 get_playerlist <- function(){
-  read.csv("http://crunchtimebaseball.com/master.csv", stringsAsFactors = F)
+  # read.csv("http://crunchtimebaseball.com/master.csv", stringsAsFactors = F)
+  data.table::fread("https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv", stringsAsFactors = F, data.table = F)
 }
